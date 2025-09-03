9f5bab2f5a7093a8a8363779456f34ae1745f87d
jiiquu <74710058+jiiquu@users.noreply.github.com>
2025-09-03 17:55:51 +0300

    Minor readability improvements

--- JS/mybutton.js
+++ JS/mybutton.js
@@ -7,8 +7,8 @@ myButton.addEventListener('click', function() {
     let tervehdys = document.getElementById('tervehdys');
     tervehdys.innerText = 'Moi maailma';
 });
+
 addData.addEventListener('click', function() {
-    
     const myList = document.getElementById('my-list');
     let newItem = document.createElement("li");
     newItem.innerText = ++itemCounter + ". " + document.getElementById('item').value;
